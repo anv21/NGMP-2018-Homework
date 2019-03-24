71f6bd9039e07ae950df0237500baf030843c9c3
Anastasiya <novakanastasya@yandex.ru>
2019-03-24 18:32:17 +0300

    Homework-8: SWAGGER.DOCS

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import expressSession from 'express-session';
 import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import yamljs from 'yamljs';
 import routes from './routes/routes';
 import passport from "./auth/auth-strategies";
 import {parserCookie} from './middlewares/cookie-parser';
 import {queryParser} from './middlewares/query-parser';
 
 const app = express();
+const swaggerDoc = yamljs.load('./swagger.yaml');
 
 app.use(queryParser);
 app.use(parserCookie);
@@ -15,5 +18,6 @@ app.use(expressSession({secret: 'SECRET', resave: true, saveUninitialized: true}
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/', routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 export default app;
